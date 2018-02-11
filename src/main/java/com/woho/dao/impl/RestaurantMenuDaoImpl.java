0a17b8e2773916aaa1a0284ce0aa5eb5f884e255
Arpan <arpan.khagram@pathcom.com>
2018-02-11 12:53:41 -0500

    [ARPAN] Food Menu related

--- src/main/java/com/woho/dao/impl/RestaurantMenuDaoImpl.java
+++ src/main/java/com/woho/dao/impl/RestaurantMenuDaoImpl.java
@@ -1,9 +1,11 @@
 package com.woho.dao.impl;
 
+import org.springframework.stereotype.Repository;
 import com.woho.dao.AbstractDAO;
 import com.woho.dao.RestaurantMenuDao;
 import com.woho.model.RestaurantMenu;
 
+@Repository
 public class RestaurantMenuDaoImpl extends AbstractDAO<RestaurantMenu> implements RestaurantMenuDao{
 
 	/**
--- src/main/java/com/woho/model/RestaurantMenu.java
+++ src/main/java/com/woho/model/RestaurantMenu.java
@@ -2,31 +2,41 @@ package com.woho.model;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="RESTAURANT_MENU")
 public class RestaurantMenu {
 	
-	private Long RESTAURANT_MENU_ID;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "RESTAURANTMENUID")
+	private Long restaurantMenuId;
 	
+	@OneToMany(cascade = CascadeType.ALL) 
 	private Set<MenuItem> menuItems;
-
-	public Long getRESTAURANT_MENU_ID() {
-		return RESTAURANT_MENU_ID;
+	
+	public Long getRestaurantMenuId() {
+		return restaurantMenuId;
 	}
 
-	public void setRESTAURANT_MENU_ID(Long rESTAURANT_MENU_ID) {
-		RESTAURANT_MENU_ID = rESTAURANT_MENU_ID;
+	public void setRestaurantMenuId(Long restaurantMenuId) {
+		this.restaurantMenuId = restaurantMenuId;
+	}
+
+	public void setMenuItems(Set<MenuItem> menuItems) {
+		this.menuItems = menuItems;
 	}
 
 	public Set<MenuItem> getMenuItems() {
 		return menuItems;
-	}
-
-	public void setMenuItemsVO(Set<MenuItem> menuItems) {
-		this.menuItems = menuItems;
 	}
 	
 }
